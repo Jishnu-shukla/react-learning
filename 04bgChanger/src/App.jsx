b18e4c2ba1e7a46e1a69576582594565b3fb5ce4
Jishnu Shukla <jishnushukla9695@gmail.com>
2026-04-18 09:50:01 +0530

    Made 4th project

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("white");
+
+  return (
+    <div
+    style={{
+      backgroundColor: color,
+      width: "100%",
+      minHeight: "100vh",
+    }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl">
+          <button 
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white font-medium shadow-lg cursor-pointer"
+          style={{
+            backgroundColor: "red"
+          }}
+          >Red</button>
+          <button 
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white font-medium shadow-lg cursor-pointer"
+          style={{
+            backgroundColor: "green"
+          }}
+          >Green</button>
+          <button 
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white font-medium shadow-lg cursor-pointer"
+          style={{
+            backgroundColor: "blue"
+          }}
+          >Blue</button> 
+          <button 
+          onClick={() => setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full font-medium shadow-lg cursor-pointer"
+          style={{
+            backgroundColor: "yellow"
+          }}
+          >Yellow</button>
+          <button 
+          onClick={() => setColor("pink")}
+          className="outline-none px-4 py-1 rounded-full font-medium shadow-lg cursor-pointer"
+          style={{
+            backgroundColor: "pink"
+          }}
+          >Pink</button>
+          <button 
+          onClick={() => setColor("magenta")}
+          className="outline-none px-4 py-1 rounded-full text-white font-medium shadow-lg cursor-pointer"
+          style={{
+            backgroundColor: "magenta"
+          }}
+          >Magenta</button>
+          <button 
+          onClick={() => setColor("olive")}
+          className="outline-none px-4 py-1 rounded-full text-white font-medium shadow-lg cursor-pointer"
+          style={{
+            backgroundColor: "olive"
+          }}
+          >Olive</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
